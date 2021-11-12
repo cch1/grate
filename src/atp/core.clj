4b9473e3193db67edf0786d3d05633451a41c6cf
Chris Hapgood <cch1@hapgood.com>
2021-11-12 06:08:55 -0500

    Add Event04 Solution

--- src/atp/core.clj
+++ src/atp/core.clj
@@ -37,3 +37,30 @@
         record (nth db record-index)]
     (prn ((zipmap (first db) record) (str field-name)))))
 
+(defn event04
+  [& args]
+  (let [[[epoch-start epoch-end] results] (db/load *in*)
+        num-weeks (inc (quot (- (.toEpochDay epoch-end) (.toEpochDay epoch-start)) 7))
+        weeks (take num-weeks (iterate advance-week epoch-start))
+        results-by-week (group-by :week-of results)
+        weekly-results (sequence (map (fn [week] [week (results-by-week week ())])) weeks)
+        [ampw mmpw pactivity] (reduce (fn [acc [week results]] (let [n (count results)
+                                                                     winners (frequencies (map :winner results))
+                                                                     losers (frequencies (map :loser results))]
+                                                                 (-> acc
+                                                                     (update 0 + n)
+                                                                     (update 1 (fn [max] (if (> max n) max n)))
+                                                                     (update 2 (fn [pactivity] (merge-with + pactivity winners losers))))))
+                                      [0 0 {}]
+                                      weekly-results)
+        most-actives (last (reduce (fn [[max-matches most-actives] [player matches]] (case (int (Math/signum (float (compare matches max-matches))))
+                                                                                       -1 [max-matches most-actives]
+                                                                                       0 [max-matches (conj most-actives player)]
+                                                                                       1 [matches [player]]))
+                                   [0 []]
+                                   pactivity))]
+    (println (str epoch-start)
+             (str epoch-end)
+             (int (/ ampw num-weeks))
+             mmpw
+             most-actives)))
